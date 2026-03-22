bd66634e69aafc2c80506c42e73f1f0361453589
Dipen Boro <borod9200@gmail.com>
2026-03-22 19:09:00 +0000

    http with express

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const app = require('./src/app');
+const http = require('http');
 const  connectToDatabase  = require('./src/config/db');
 
 const PORT = process.env.PORT || 8000;
+
+const server = http.createServer(app);
 
 const startServer = async () => {
     try {
         await connectToDatabase()
         // await redis.connect();
-        app.listen(PORT, () => {
+        server.listen(PORT, () => {
             console.log(`Server is running on port ${PORT}`);
         });
     } catch (error) {
