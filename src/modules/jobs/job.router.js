22f4a34152072e45ebd7775e5959fb217cc014a9
Dipen Boro <borod9200@gmail.com>
2026-03-26 10:19:27 +0000

    adding single job router

--- src/modules/jobs/job.router.js
+++ src/modules/jobs/job.router.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const { protect, authorize } = require('../../middleware/auth')
-const { create, list } = require('./job.controller');
+const { create, list , singleJob } = require('./job.controller');
 const { createJobValidator, updateJobValidator, paginationValidator } = require('./job.validator');
 
 router.post('/', protect, authorize('recruiter'), createJobValidator, create);
 router.get('/', paginationValidator ,list);
+router.get('/:id', singleJob);
 
 module.exports = router;
