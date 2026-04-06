22ef1aedb102836a346351ce00b614d18063441d
Dipen Boro <borod9200@gmail.com>
2026-04-06 09:05:02 +0000

    added httpstatus

--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -1,8 +1,7 @@
-const { validationResult } = require('express-validator');
 const { loginUser, registerUser, refreshAccessToken } = require('./auth.service');
 const  asyncHandler = require('../../utils/asyncHandler')
-const ApiError = require('../../utils/ApiError');
 const ApiResponse = require('../../utils/ApiResponse');
+const { HTTP_STATUS } = require('../../constants/httpStatus');
 
 
 const register = asyncHandler(async (req, res) => {
@@ -14,23 +13,23 @@ const register = asyncHandler(async (req, res) => {
         sameSite: 'strict',
         maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
     });
-    res.status(201).json(new ApiResponse(201, 'Registration Successfull', result));
+    res.status(HTTP_STATUS.CREATED).json(new ApiResponse(201, 'Registration Successfull', result));
 })
 
 const login = asyncHandler(async (req, res) => {
     const result = await loginUser(res, req.body);
-    res.status(200).json(new ApiResponse(200, "Login Successfull", result));
+    res.status(HTTP_STATUS.OK).json(new ApiResponse(200, "Login Successfull", result));
 })
 
 const refreshToken = asyncHandler(async (req, res) => {
     const refreshToken = req.cookies.refreshToken;
     const result = await refreshAccessToken(res,refreshToken);
 
-    res.status(200).json(new ApiResponse(200, "Token Refreshed", result));
+    res.status(HTTP_STATUS.OK).json(new ApiResponse(200, "Token Refreshed", result));
 })
 
 const getMe = asyncHandler(async(req,res)=> {
-    res.status(200).json(new ApiResponse(200, "fetch user", req.user));
+    res.status(HTTP_STATUS.OK).json(new ApiResponse(200, "fetch user", req.user));
 })
 
 module.exports = { register, login, getMe, refreshToken };
--- src/modules/jobs/job.controller.js
+++ src/modules/jobs/job.controller.js
@@ -1,42 +1,43 @@
 const ApiResponse = require('../../utils/ApiResponse');
 const asyncHandler = require('../../utils/asyncHandler');
+const { HTTP_STATUS } = require('../../constants/httpStatus');
 
 const { createJob, getJobs, getJobById, searchJobs, updateJobs, deleteJob, getStats } = require('./job.service');
 
 const create = asyncHandler(async (req, res) => {
     const job = await createJob(req.body, req.user._id);
-    res.status(201).json(new ApiResponse(201, 'Job Created ', job));
+    res.status(HTTP_STATUS.CREATED).json(new ApiResponse(201, 'Job Created ', job));
 })
 
 const list = asyncHandler(async (req, res) => {
     const { limit, cursor, search, location, status, tag, skills, jobType, workMode } = req.query;
     const result = await getJobs({ limit, cursor, search, location, jobType, tag, status, skills, workMode });
-    res.status(200).json(new ApiResponse(200, 'Job Fetched', result.jobs  ,{ nextCursor: result.nextCursor, totalJobs: result.totalJobs }));
+    res.status(HTTP_STATUS.OK).json(new ApiResponse(200, 'Job Fetched', result.jobs  ,{ nextCursor: result.nextCursor, totalJobs: result.totalJobs }));
 });
 
 const singleJob = asyncHandler(async (req, res) => {
     const job = await getJobById(req.params.id);
-    res.status(200).json(new ApiResponse(200, "fetch single job", job));
+    res.status(HTTP_STATUS.OK).json(new ApiResponse(200, "fetch single job", job));
 });
 
 const search = asyncHandler(async (req, res) => {
     const result = await searchJobs(req.query);
-    res.status(200).json(new ApiResponse(200, "Search result", result.jobs, {total: result.total}));
+    res.status(HTTP_STATUS.OK).json(new ApiResponse(200, "Search result", result.jobs, {total: result.total}));
 })
 
 const update = asyncHandler(async (req, res) => {
     const job = await updateJobs(req.params.id, req.body, req.user._id);
-    res.status(200).json(new ApiResponse(200, "updated job successfully", job));
+    res.status(HTTP_STATUS.OK).json(new ApiResponse(200, "updated job successfully", job));
 });
 
 const deletejob = asyncHandler(async(req, res) => {
     await deleteJob(req.params.id, req.user._id);
-    res.status(200).json(new ApiResponse(200, "job deleted"));
+    res.status(HTTP_STATUS.OK).json(new ApiResponse(200, "job deleted"));
 })
 
 const stats = asyncHandler(async(req, res) => {
     const data = await getStats();  
-    res.status(200).json(new ApiResponse(200, 'Stats fetched', data));
+    res.status(HTTP_STATUS.OK).json(new ApiResponse(200, 'Stats fetched', data));
 });
 
 module.exports = { create, list, singleJob, search, update, deletejob, stats };
